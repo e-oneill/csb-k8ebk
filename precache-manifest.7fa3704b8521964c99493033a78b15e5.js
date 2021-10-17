ed5b840460e01d73736e3a6d3973b92c68c8b9df
Code Sandbox <hello@codesandbox.io>
2021-10-17 10:49:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7fa3704b8521964c99493033a78b15e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d96522fa0fc8cd6ead6b1b03c5ea586",
+    "url": "https://e-oneill.github.io/csb-k8ebk/index.html"
+  },
+  {
+    "revision": "78ea3b95cff8c4631155",
+    "url": "https://e-oneill.github.io/csb-k8ebk/static/css/2.0a596a0f.chunk.css"
+  },
+  {
+    "revision": "bce41919fef0796ba2f4",
+    "url": "https://e-oneill.github.io/csb-k8ebk/static/css/main.e07d6952.chunk.css"
+  },
+  {
+    "revision": "78ea3b95cff8c4631155",
+    "url": "https://e-oneill.github.io/csb-k8ebk/static/js/2.f3d853f4.chunk.js"
+  },
+  {
+    "revision": "bce41919fef0796ba2f4",
+    "url": "https://e-oneill.github.io/csb-k8ebk/static/js/main.b51eeea8.chunk.js"
+  },
+  {
+    "revision": "008300eb0242a8844293",
+    "url": "https://e-oneill.github.io/csb-k8ebk/static/js/runtime~main.f187eaa8.js"
+  }
+]);
