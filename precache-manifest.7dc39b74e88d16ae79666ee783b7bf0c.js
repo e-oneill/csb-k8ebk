4167b3261a08dcbdaaeba3abe52664b986274dd5
Code Sandbox <hello@codesandbox.io>
2021-12-21 17:59:15 +0000

    Deployment from Codesandbox.

--- precache-manifest.7dc39b74e88d16ae79666ee783b7bf0c.js
+++ precache-manifest.7dc39b74e88d16ae79666ee783b7bf0c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0e30f589cbac7d5010aa4b1a431c35e9",
+    "revision": "e201c42d3562180472d25b4ab0677059",
     "url": "https://e-oneill.github.io/csb-k8ebk/index.html"
   },
   {
-    "revision": "b1cc20f1180003b2d83d",
+    "revision": "8022fa9d54724c7daf27",
     "url": "https://e-oneill.github.io/csb-k8ebk/static/css/2.0a596a0f.chunk.css"
   },
   {
-    "revision": "84a8dfa5f4ae69ff4821",
+    "revision": "12217d0fa26722e743d9",
     "url": "https://e-oneill.github.io/csb-k8ebk/static/css/main.3155712c.chunk.css"
   },
   {
-    "revision": "b1cc20f1180003b2d83d",
-    "url": "https://e-oneill.github.io/csb-k8ebk/static/js/2.83873dec.chunk.js"
+    "revision": "8022fa9d54724c7daf27",
+    "url": "https://e-oneill.github.io/csb-k8ebk/static/js/2.60eeea50.chunk.js"
   },
   {
-    "revision": "84a8dfa5f4ae69ff4821",
-    "url": "https://e-oneill.github.io/csb-k8ebk/static/js/main.d3615332.chunk.js"
+    "revision": "12217d0fa26722e743d9",
+    "url": "https://e-oneill.github.io/csb-k8ebk/static/js/main.7ed0f8f6.chunk.js"
   },
   {
     "revision": "008300eb0242a8844293",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://e-oneill.github.io/csb-k8ebk/precache-manifest.dc1c560a1d943e6640157850c09a78f4.js"
+  "https://e-oneill.github.io/csb-k8ebk/precache-manifest.7dc39b74e88d16ae79666ee783b7bf0c.js"
 );
 
 self.addEventListener('message', (event) => {
